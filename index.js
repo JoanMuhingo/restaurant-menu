4396acb4864aebc83468bba2b82f74342c9e50bc
Joan <joanmuhingo@gmail.com>
2023-11-02 21:30:43 +0300

    created a js function to get menulist by searchng main ingredient

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 
-// const searchButton = document.getElementById('search-button');
-// const mealList = document.getElementById('meal');
+const searchButton = document.getElementById('search-button');
+const mealList = document.getElementById('meal');
 
-// searchButton.addEventListener('click', getMealList);
+searchButton.addEventListener('click', getMealList);
 
-// function getMealList(){
-//     let searchTextArea = document.getElementById('search').value.trim();
+function getMealList(){
+    let searchTextArea = document.getElementById('search').value.trim();
 
-//     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTextArea}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data);
-//             let html= "" ;
-//             if (data.meals){
-//                 data.meals.forEach(meal => {
-//                     html =+ `
-//                     <div class = " ${meal.strMealThumb}">
-//         <div class ="">
-//             <h3> ${meal.strMeal}</h3>
-//         <a href ="#" class=" recipe-button"> Recipe</a>
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTextArea}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let html= "" ;
+            if (data.meals){
+                data.meals.forEach(meal => {
+                    html =+ `
+                    <div class = " ${meal.strMealThumb}">
+        <div class ="">
+            <h3> ${meal.strMeal}</h3>
+        <a href ="#" class=" recipe-button"> Recipe</a>
                     
-//                     `
+                    `
                     
-//                 });
-//             }
-//             else{
-//                 html =' oops we do not have that';
-//             }
-//             mealList.innerHTML = html;
-//         })
-//         .catch(error => {
-//             console.error("Error fetching data:", error);
-//         });
-// };
+                });
+            }
+            else{
+                html =' oops we do not have that';
+            }
+            mealList.innerHTML = html;
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+};
 //  mealList.addEventListener('click',function (e){
 //     e.preventDefault();
 //     if(e.target.classList.contains('recipe-button')){
