18f650f2396493cab68392c027ed98acfb0be868
Joan <joanmuhingo@gmail.com>
2023-11-02 21:32:33 +0300

    created a js function that displays the recipe of meals that are displayed by menulist function

--- index.js
+++ index.js
@@ -33,22 +33,22 @@ function getMealList(){
             console.error("Error fetching data:", error);
         });
 };
-//  mealList.addEventListener('click',function (e){
-//     e.preventDefault();
-//     if(e.target.classList.contains('recipe-button')){
-//         const mealId = e.target.getAttribute('data-id');
-//         getrecipe(mealId);
-//     }
-// })
-// function getRecipe(){
-//     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}` )
-//     .then(response => response.json())
-//     .then(data=> {
-//         const meal = data.meals[0];
-//         const recipe =meal.strInstructions;
-//         alert(recipe); 
-//  })
-//  .catch(error => {
-//     console.error("Error fetching recipe data:", error);
-// });
-// }
+ mealList.addEventListener('click',function (e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-button')){
+        const mealId = e.target.getAttribute('data-id');
+        getrecipe(mealId);
+    }
+})
+function getRecipe(){
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}` )
+    .then(response => response.json())
+    .then(data=> {
+        const meal = data.meals[0];
+        const recipe =meal.strInstructions;
+        alert(recipe); 
+ })
+ .catch(error => {
+    console.error("Error fetching recipe data:", error);
+});
+}
