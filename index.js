297354eea6ea16b1e2906a84361fd9f375db073b
Joan <joanmuhingo@gmail.com>
2023-11-04 18:54:28 +0300

    the meal list functionality was modified and is working

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded',function(e){
+    const homepage =document.getElementById('meal');
     fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            if(data.meals){
+                const list=data.meals;
+                const html =list.map(meal =>{
+                    return `
+                    <div class = " meal-img">
+                    <h2>${meal.strMeal}</h2>    
+            <img  src="${meal.strMealThumb}" alt="#"> 
+        </div>
+        <a href ="#" class=" recipe-button"> Recipe</a>
+                    `;
+                }).join('');
+                homepage.innerHTML = html;
+            }
+            else{
+                homepage.innerHTML = " OOPS!";
+            }
             
         })
         .catch(error => {
@@ -10,6 +26,7 @@ document.addEventListener('DOMContentLoaded',function(e){
         });
         
 })
+
 const searchButton = document.getElementById('search-button');
 const mealList = document.getElementById('meal');
 
@@ -17,23 +34,25 @@ searchButton.addEventListener('click', getMealList);
 
 function getMealList(){
     let searchTextArea = document.getElementById('search').value.trim();
-
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchTextArea}`)
         .then(response => response.json())
         .then(data => {
+
             console.log(data);
-            let html= "" ;
+            let html= " " ;
             if (data.meals){
                 data.meals.forEach(meal => {
-                    html =+ `
-                    <div class = " ${meal.strMealThumb}">
-        <div class ="">
-            <h3> ${meal.strMeal}</h3>
-        <a href ="#" class=" recipe-button"> Recipe</a>
+                    html += `
+                    <div class = " meal-img">
+                    <h2>${meal.strMeal}</h2>    
+            <img  src="${meal.strMealThumb}" alt="${meal.strMeal}"> 
+        </div>
+        <a href ="#" class="recipe-button" data id="${meal.idMeal}"}"> Recipe</a>
                     
-                    `
-                    
-                });
+                    `  
+               
+     }
+        );
             }
             else{
                 html =' oops we do not have that';
@@ -48,18 +67,27 @@ function getMealList(){
     e.preventDefault();
     if(e.target.classList.contains('recipe-button')){
         const mealId = e.target.getAttribute('data-id');
-        getrecipe(mealId);
+        getRecipe(mealId);
     }
 })
-function getRecipe(){
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}` )
-    .then(response => response.json())
-    .then(data=> {
-        const meal = data.meals[0];
-        const recipe =meal.strInstructions;
-        alert(recipe); 
- })
- .catch(error => {
-    console.error("Error fetching recipe data:", error);
-});
+function getRecipe(mealId) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.meals && data.meals.length > 0) {
+                const meal = data.meals[0];
+                const recipe = meal.strInstructions;
+                displayRecipe(recipe);
+            } else {
+                console.error("Recipe not available for meal ID:", mealId);
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching recipe data:", error);
+        });
+}
+
+function displayRecipe(recipe) {
+    
+    alert(recipe);
 }
